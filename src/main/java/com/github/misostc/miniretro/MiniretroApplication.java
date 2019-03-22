b2cba7ef4804e28460b446a150d09bc4f00e9d10
Michal Bábel <miso@outlook.sk>
2019-03-22 21:49:02 +0100

    Init empty project

---
+++ src/main/java/com/github/misostc/miniretro/MiniretroApplication.java
@@ -0,0 +1,13 @@
+package com.github.misostc.miniretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniretroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniretroApplication.class, args);
+	}
+
+}
