4fb55e79589ae7cfaab07da5d5c5490882d19e20
Michal Bábel <miso@outlook.sk>
2019-03-23 16:07:53 +0100

    UI: setup redux, routing, thunk, devtools

--- src/main/app/src/App.tsx
+++ src/main/app/src/App.tsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, Route } from "react-router";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from "./configure-store";
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <>
+            <Route path="/">
+              <h1>Hello world!</h1>
+            </Route>
+          </>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
